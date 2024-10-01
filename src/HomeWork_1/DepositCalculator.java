d30e4b4d2c2ce993f5300f2e5a78fd0035a381f8
ShakirKadirov <shakirkadirov1@gmail.com>
2024-10-01 19:59:28 +0500

    Task DepositCalculator completed

---
+++ src/HomeWork_1/DepositCalculator.java
@@ -0,0 +1,24 @@
+package HomeWork_1;
+import java.util.Scanner;
+
+public class DepositCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите начальный депозит: ");
+        double deposit = scanner.nextDouble();
+
+        System.out.print("Введите процентную ставку (в процентах): ");
+        double procent = scanner.nextDouble();
+
+        System.out.print("Введите количество лет: ");
+        int years = scanner.nextInt();
+
+        double finalAmount = deposit * Math.pow(1 + procent / 100, years);
+
+        System.out.printf("Сумма на депозите после %d лет: %.2f%n", years, finalAmount);
+
+        scanner.close();
+    }
+}
+
